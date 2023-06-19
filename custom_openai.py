901c1fe69075bf6a0b6de91fd8470a98bc8fbe11
Melydron <petrosha@gmail.com>
2023-06-20 00:57:01 +0300

    WIP

--- custom_openai.py
+++ custom_openai.py
@@ -9,15 +9,16 @@ openai.api_key = os.getenv('CHATGPT_API_KEY')
 def chatgpt_response(prompt):
     try:
         # Sending a request to the ChatGPT API
-        response = openai.ChatCompletion.create(
+        response = openai.Completion.create(
             engine="gpt-3.5-turbo",  # Specify the model
-            prompt=prompt,           # Input text
-            temperature=0.7,         # Controls randomness: Higher values (closer to 1) make output more random, lower values make it more focused
-            max_tokens=50            # Limit the length of the output text
+            messages=[{"role": "user", "content": prompt}]
+            #prompt=prompt,           # Input text
+            #temperature=0.7,         # Controls randomness: Higher values (closer to 1) make output more random, lower values make it more focused
+            #max_tokens=50            # Limit the length of the output text
         )
         
         # Extracting the text from the response
-        return response.choices[0].text
+        return response.choices[0].message.content
 
     except Exception as e:
         error_str = f"An error occurred: {e}"
